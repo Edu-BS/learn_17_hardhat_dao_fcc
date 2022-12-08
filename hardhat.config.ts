801f0de1e6328564430c0456e3ed854ca8bf439e
Edu-BS <edu.bs.email@gmail.com>
2022-12-08 19:17:55 +0100

    Add SmartContract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,9 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
+                version: "0.8.8",
+            },
+            {
                 version: "0.8.7",
             },
             {

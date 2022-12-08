587dd7b7f1d443255e2648ef366d14c00f9626e7
Edu-BS <edu.bs.email@gmail.com>
2022-12-08 18:32:50 +0100

    Hardhat init

---
+++ hardhat.config.ts
@@ -0,0 +1,76 @@
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "solidity-coverage"
+import { HardhatUserConfig } from "hardhat/config"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                chainId: 5,
+                network: "goerli",
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, 
+            1: 0,
+        },
+        user1: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 200000,
+    },
+}
+
+export default config
---
+++ helper-hardhat-config.ts
@@ -0,0 +1,54 @@
+export interface networkConfigItem {
+    name?: string
+    wethToken?: string
+    lendingPoolAddressesProvider?: string
+    daiEthPriceFeed?: string
+    daiToken?: string
+    blockConfirmations?: number
+    ethUsdPriceFeed?: string
+    vrfCoordinatorV2?: string
+    subscriptionId?: string
+    gasLane?: string
+    mintFee?: string
+    callbackGasLimit?: string
+    linkToken?: string
+}
+
+export interface networkConfigInfo {
+    [key: string]: networkConfigItem
+}
+
+export const networkConfig: networkConfigInfo = {
+    31337: {
+        name: "localhost",
+        wethToken: "",
+        lendingPoolAddressesProvider: "",
+        daiEthPriceFeed: "",
+        daiToken: "",
+        mintFee: "", // 0.01 ETH
+        callbackGasLimit: "", // 500,000 gas
+        gasLane: "",
+        blockConfirmations: 1,
+    },
+    5: {
+        name: "goerli",
+        // Link ---
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        linkToken: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e", // Chainlink price feed contract address
+        subscriptionId: "",
+        // ---
+        blockConfirmations: 3,
+
+    },
+    1: {
+
+    }
+}
+
+export const developmentChains = ["hardhat", "localhost"]
+export const DECIMALS = "18"
+export const INITIAL_PRICE = "200000000000000000000"
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+export const frontEndContractsFile = "../nextjs-nft-marketplace-thegraph-fcc/constants/networkMapping.json"
+export const frontEndAbiLocation = "../nextjs-nft-marketplace-thegraph-fcc/constants/"

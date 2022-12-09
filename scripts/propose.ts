64a6991e58bac6dacfcc854b37a748e07c7c1acf
Edu-BS <edu.bs.email@gmail.com>
2022-12-09 16:36:55 +0100

    Implement hardhat-network-helpers to mine blocks
    Now the script uses the mine() function from hardhat-network-helpers,
    because the function network.provider.send() didn't work.
    Improve the propose.ts script.

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,10 +1,21 @@
 import { ethers, network } from "hardhat"
-import { developmentChains, FUNC, NEW_STORE_VALUE, proposalsFile, PROPOSAL_DESCRIPTION, VOTING_DELAY } from "../helper-hardhat-config"
+import {
+    developmentChains,
+    FUNC,
+    NEW_STORE_VALUE,
+    proposalsFile,
+    PROPOSAL_DESCRIPTION,
+    VOTING_DELAY,
+} from "../helper-hardhat-config"
 import { Box, GovernorContract } from "../typechain-types"
 import { moveBlocks } from "../utils/move-blocks"
-import * as fs from "fs" 
+import * as fs from "fs"
+import { mine } from "@nomicfoundation/hardhat-network-helpers"
 
 export async function propose(args: any[], functionToCall: string, proposalDescription: string) {
+    let values, proposals
+    const chainId = network.config.chainId!.toString()
+
     const governor: GovernorContract = await ethers.getContract("GovernorContract")
     const box: Box = await ethers.getContract("Box")
     // @ts-ignore
@@ -12,27 +23,37 @@ export async function propose(args: any[], functionToCall: string, proposalDescr
 
     console.log(`Proposing ${functionToCall} on ${box.address} with ${args}`)
     console.log(`Proposal description: \n ${proposalDescription}`)
+
+    values = getProposals(proposalsFile)[chainId].length
+    
     const proposeTx = await governor.propose(
         [box.address],
-        [0],
+        [values],
         [encodedFunctionCall],
         proposalDescription
     )
     const proposeTxReceipt = await proposeTx.wait(1)
 
     if (developmentChains.includes(network.name)) {
-        moveBlocks(VOTING_DELAY + 1)
+        await mine(VOTING_DELAY + 1)
     }
 
-    const proposalId = proposeTxReceipt.events![0].args!.proposalId;
+    const proposalId = proposeTxReceipt.events![0].args!.proposalId
+    proposals = getProposals(proposalsFile)
+
+    proposals[chainId].push(proposalId.toString())
+    fs.writeFileSync(proposalsFile, JSON.stringify(proposals, null, 4))
+}
+
+function getProposals(proposalsFile: string) {
+    let proposals
     if (!fs.existsSync(proposalsFile)) {
-        let emptyProposal = {"31337": []}
+        let emptyProposal = { "31337": [] }
         fs.writeFileSync(proposalsFile, JSON.stringify(emptyProposal), "utf-8")
     }
-    let proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf-8"))
+    proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf-8"))
 
-    proposals[network.config.chainId!.toString()].push(proposalId.toString())
-    fs.writeFileSync(proposalsFile, JSON.stringify(proposals))
+    return proposals
 }
 
 propose([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
